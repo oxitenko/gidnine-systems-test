21ee973a9618febc291785055a8318edda6454ab
Oksana Komarova <rabota_komarova@mail.ru>
2023-12-24 15:32:36 +0700

    feat(card): finish card component

--- src/Card/Card.js
+++ src/Card/Card.js
@@ -12,19 +12,19 @@ const Card = ({ card }) => {
     <li className="mb-8 w-full">
       <div className="flex justify-between h-min bg-sky-600 text-white p-2 items-center">
         <p>{card.flight.carrier.caption}</p>
-        <div className="flex flex-col justify-end w-4/5 items-end">
+        <div className="flex flex-col justify-end w-[60%] items-end">
           <p>{card.flight.price.total.amount} ₽</p>
           <p className="text-xs">Стоимость для одного взрослого пассажира</p>
         </div>
       </div>
 
-      <div className="w-11/12 m-auto">
+      <div className="w-[95%] m-auto">
         {card.flight.legs.map((leg, index) => (
           <div key={index}>
             {leg.segments.map((segment, index) => (
               <>
                 <div
-                  className="flex text-base w-max font-semibold border-b border-b-stone-300 h-8 my-3"
+                  className="flex text-base w-full font-semibold border-b border-b-stone-200 h-8 my-3"
                   key={index}
                 >
                   <p>
@@ -43,7 +43,6 @@ const Card = ({ card }) => {
                     </span>
                   </p>
                 </div>
-
                 <div className="flex w-full justify-between">
                   <div className="flex items-end">
                     <p className="font-semibold mr-2">
@@ -57,7 +56,6 @@ const Card = ({ card }) => {
                         .toFormat('d LLL EEE.')}
                     </p>
                   </div>
-
                   <p className="flex items-center">
                     <FaRegClock style={{ marginRight: '5px' }} />
                     {formatDuration(segment?.travelDuration)}
@@ -76,10 +74,20 @@ const Card = ({ card }) => {
                     </p>
                   </div>
                 </div>
-
-                <span>{segment?.stops} пересадка</span>
-
-                <p className="block border-b-2 border-b-sky-700">
+                <div className="flex items-center w-11/12 m-auto mt-2 mb-4 h-6">
+                  {segment?.stops === 0 ? (
+                    <div className="flex-1 h-0.5 border-t border-t-stone-400"></div>
+                  ) : (
+                    <>
+                      <div className="flex-1 h-0.5 border-t border-t-stone-400"></div>
+                      <span className="mx-2 text-orange-400">
+                        {segment?.stops} пересадка
+                      </span>
+                      <div className="flex-1 h-0.5 border-t border-t-stone-400"></div>
+                    </>
+                  )}
+                </div>
+                <p className="block border-b-2 border-b-sky-700 pb-2">
                   Рейс выполняет: {segment?.airline.caption}
                 </p>
               </>
@@ -87,7 +95,10 @@ const Card = ({ card }) => {
           </div>
         ))}
       </div>
-      <button className="w-full bg-orange-300 text-white h-10 mt-4">
+      <button
+        type="button"
+        className="w-full bg-orange-300 text-white h-10 mt-4"
+      >
         ВЫБРАТЬ
       </button>
     </li>
